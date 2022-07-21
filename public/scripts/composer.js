34771144e3984c94c52dfc3c7d489447bd725ee6
Adam <adamgrharvey@gmail.com>
2022-07-21 15:10:05 -0700

    more tidy up, front end slight changes

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
 
-  function scrollFunction() {
+  const scrollFunction = function() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
      mybutton.style.display = "block";
     } else {
@@ -16,9 +16,8 @@ $(document).ready(function() {
   }
 
 // When the user clicks on the button, scroll to the top of the document
-const topFunction = function() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
+  const topFunction = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 });
